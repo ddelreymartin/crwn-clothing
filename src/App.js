9c1718eb51e9919156a88fcd3efcdaf7a92d0ad4
David <ddelreymartin@DDELREY-DELL.stacks-cis.ct>
2020-11-04 19:33:52 +0100

    Adding routing, and pass linkUrl to items components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
-import "./App.css";
-import Homepage from "./pages/homepage/homepage.component";
+import React from 'react';
+import './App.css';
+import Homepage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => {
+	return (
+		<div>
+			<h1>HATS PAGE</h1>
+		</div>
+	);
+};
 
 function App() {
-  return (
-    <div>
-      <Homepage />
-    </div>
-  );
+	return (
+		<div>
+			<Switch>
+				<Route exact path="/" component={Homepage} />
+				<Route path="/shop/hats" component={HatsPage} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
